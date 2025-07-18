d07b42aa1c18205558d7457dc1804db92155b0bd
Patricky Lucas <patrickylucas@hotmail.com>
2025-07-18 18:53:43 -0300

    adicionando classes de Controller Pessoa

---
+++ src/main/java/br/com/dio/enderecopessoas/controller/PessoaController.java
@@ -0,0 +1,32 @@
+package br.com.dio.enderecopessoas.controller;
+
+import br.com.dio.enderecopessoas.dto.PessoaRequestDTO;
+import br.com.dio.enderecopessoas.dto.PessoaResponseDTO;
+import br.com.dio.enderecopessoas.service.PessoaService;
+import jakarta.validation.Valid;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    private final PessoaService service;
+
+    public PessoaController(PessoaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<PessoaResponseDTO> salvar(@RequestBody @Valid PessoaRequestDTO dto) {
+        PessoaResponseDTO response = service.salvar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public List<PessoaResponseDTO> listar() {
+        return service.listarTodos();
+    }
+}
