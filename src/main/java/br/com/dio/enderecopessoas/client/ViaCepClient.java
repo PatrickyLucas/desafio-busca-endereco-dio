08d0fbbbc203895809f39ae9f4c6f4afb5ce88b4
Patricky Lucas <patrickylucas@hotmail.com>
2025-07-18 18:42:09 -0300

    adicionando classes de client viacep atraves do feingclient

---
+++ src/main/java/br/com/dio/enderecopessoas/client/ViaCepClient.java
@@ -0,0 +1,11 @@
+package br.com.dio.enderecopessoas.client;
+
+import br.com.dio.enderecopessoas.dto.EnderecoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepClient {
+    @GetMapping("/{cep}/json/")
+    EnderecoDTO buscar(@PathVariable("cep") String cep);
+}
