8f4717e23aedd56ca165af95217b4414f8f7688d
Patricky Lucas <patrickylucas@hotmail.com>
2025-07-18 19:04:56 -0300

    habilitando feing do viacep no projeto na classe de execução main

--- src/main/java/br/com/dio/enderecopessoas/EnderecopessoasApplication.java
+++ src/main/java/br/com/dio/enderecopessoas/EnderecopessoasApplication.java
@@ -2,12 +2,12 @@ package br.com.dio.enderecopessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EnderecopessoasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EnderecopessoasApplication.class, args);
 	}
-
 }
