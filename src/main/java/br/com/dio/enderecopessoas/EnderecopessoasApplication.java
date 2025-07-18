b744c5b5c683a75025065098f54c1e251c64cab6
Patricky Lucas <patrickylucas@hotmail.com>
2025-07-18 18:19:56 -0300

    primeiro commit

---
+++ src/main/java/br/com/dio/enderecopessoas/EnderecopessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.enderecopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecopessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecopessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/enderecopessoas/EnderecopessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.enderecopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecopessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
